f6b50a298c50f08b007247fe146bd34e575325f1
goddi1999 <godfredasefa@icloud.com>
2023-02-16 23:13:38 +0100

    added Components, data sharing between elements

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import "./App.css";
+import Expenses from "./components/Expenses";
 
 function App() {
   return (
     <div className="App">
-        <p>Udemy</p>
+      <Expenses />
     </div>
   );
 }
